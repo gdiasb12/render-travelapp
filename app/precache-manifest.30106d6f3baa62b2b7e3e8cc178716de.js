141baec91f3b0182244478ad625dba6bf1d94d87
GabrielDias <gdiasb12@gmail.com>
2019-04-17 14:28:52 -0300

    First commit

---
+++ app/precache-manifest.30106d6f3baa62b2b7e3e8cc178716de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0165e85ddcfe89157973",
+    "url": "/static/js/main.1851ff30.chunk.js"
+  },
+  {
+    "revision": "6c84929ad3d93f7c295e",
+    "url": "/static/js/2.a05640b7.chunk.js"
+  },
+  {
+    "revision": "0165e85ddcfe89157973",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "6c84929ad3d93f7c295e",
+    "url": "/static/css/2.fcf8d163.chunk.css"
+  },
+  {
+    "revision": "6f22e24cf188abe6badb65415deabb1c",
+    "url": "/index.html"
+  }
+];
